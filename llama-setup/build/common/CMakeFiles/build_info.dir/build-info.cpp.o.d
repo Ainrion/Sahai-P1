30d52722c22a47414571d0e6c610c2c8aa3f3d8c
Ainrion <youremail@example.com>
2025-07-12 14:08:17 +0530

    Added llama-setup folder for LLaMA-3.2B setup

---
+++ llama-setup/build/common/CMakeFiles/build_info.dir/build-info.cpp.o.d
@@ -0,0 +1,2 @@
+common/CMakeFiles/build_info.dir/build-info.cpp.o: \
+  /Users/shubhamnegi/llama.cpp/build/common/build-info.cpp
---
+++ llama-setup/build/tools/perplexity/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/shubhamnegi/llama.cpp/tools/perplexity
+# Build directory: /Users/shubhamnegi/llama.cpp/build/tools/perplexity
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
